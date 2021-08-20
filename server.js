fc312c2b5caddb906bab91a2ba7f4de56b8ac4c5
Tadeu Oliveira <tadeu_oliveira@id.uff.br>
2021-08-20 09:58:50 -0300

    creates users

--- server.js
+++ server.js
@@ -1,41 +1,56 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser")
 
-app.use(bodyParser.urlencoded({ extended: "false" }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: "false" }))
+app.use(bodyParser.json())
 
-let mongoose;
+let mongoose
 require('dotenv').config()
 try {
-  mongoose = require("mongoose");
+  mongoose = require("mongoose")
 } catch (e) {
-  console.error(e);
+  console.error(e)
 }
 
-const {Schema} = mongoose;
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const {Schema} = mongoose
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const User = mongoose.model('User',new Schema({
+  username: String
+}))
 
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
-});
+})
 
 app.get("/is-mongoose-ok", function (req, res) {
   if (mongoose) {
-    res.json({ isMongooseOk: !!mongoose.connection.readyState });
+    res.json({ isMongooseOk: !!mongoose.connection.readyState })
   } else {
-    res.json({ isMongooseOk: false });
+    res.json({ isMongooseOk: false })
   }
-});
+})
 
 app.post("/api/users", function (req, res) {
-  console.log({request: req.body});
-  //let username = req.body.username;
-  res.json({});
-  return;
+  console.log({request: req.body})
+  let newuser = new User({
+    username: req.body.username 
+  })
+  newuser.save(function (err, data){
+    if(err){
+      throw err
+      return
+    }
+    res.json({
+      username: data.username,
+      _id: data._id
+    })
+    return
+  })
 })
 
 const listener = app.listen(process.env.PORT || 3000, () => {

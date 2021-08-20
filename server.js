3640ca9a05ac732e58f3a4fe4dd973b110e29920
Tadeu Oliveira <tadeu_oliveira@id.uff.br>
2021-08-20 09:30:42 -0300

    set up mongoose and environment vars

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+
+let mongoose;
 require('dotenv').config()
+try {
+  mongoose = require("mongoose");
+} catch (e) {
+  console.error(e);
+}
+
+const {Schema} = mongoose;
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,7 +19,13 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
+app.get("/is-mongoose-ok", function (req, res) {
+  if (mongoose) {
+    res.json({ isMongooseOk: !!mongoose.connection.readyState });
+  } else {
+    res.json({ isMongooseOk: false });
+  }
+});
 
 
 
